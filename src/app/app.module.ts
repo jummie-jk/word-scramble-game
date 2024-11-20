75a2afabe67b87d4573e74aa84c32ad644cf2f3f
jummie-jk <jummiejk21@gmail.com>
2024-11-20 18:52:23 +0100

    basic project set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HintComponent } from './hint/hint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    HintComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data/word-list.ts
@@ -0,0 +1,6 @@
+export const WORDS = [
+    { word: 'angular', hint: 'A popular front-end framework' },
+    { word: 'typescript', hint: 'A typed superset of JavaScript' },
+    { word: 'scramble', hint: 'To mix something up' },
+  ];
+  
